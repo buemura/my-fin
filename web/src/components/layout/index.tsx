6fc2ac5357a1ed1eb3e2fb5fd4f1c7a009257e74
Bruno <bruno.uemura1@gmail.com>
2024-01-06 13:46:16 -0300

    feat(web): adds opacity on small mode

--- web/src/components/layout/index.tsx
+++ web/src/components/layout/index.tsx
@@ -9,18 +9,24 @@ export function Layout(props: { children: JSX.Element }): JSX.Element {
 
   return (
     <div class="flex flex-col h-screen w-screen bg-neutral-950">
-      <div class="fixed w-full z-10">
+      <div class="fixed w-full z-20">
         <Header />
       </div>
 
       <div class="flex flex-grow">
         <Show when={hideSidebar()}>
-          <div class="h-screen flex-none bg-neutral-900 pt-16 fixed sm:relative">
+          <div class="h-screen flex-none bg-neutral-900 pt-16 fixed sm:relative z-10">
             <Sidebar />
           </div>
         </Show>
 
-        <div class="flex-grow overflow-y-auto pt-20">{props.children}</div>
+        <div
+          class={`flex-grow overflow-y-auto pt-20 ${
+            hideSidebar() && "opacity-35 sm:opacity-100"
+          }`}
+        >
+          {props.children}
+        </div>
       </div>
     </div>
   );
