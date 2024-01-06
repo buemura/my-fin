96029bb4c9ab23de4d5748c4abe509777be4c16c
Bruno <bruno.uemura1@gmail.com>
2024-01-06 11:15:33 -0300

    feat(web): fixes sidebar and header

--- web/src/components/layout/index.tsx
+++ web/src/components/layout/index.tsx
@@ -6,16 +6,18 @@ import { Sidebar } from "@/components/navigation/side-bar";
 export function Layout(props: { children: JSX.Element }): JSX.Element {
   return (
     <div class="flex flex-col h-screen w-screen bg-neutral-950">
-      <div class="fixed w-full">
+      <div class="fixed w-full z-10">
         <Header />
       </div>
 
       <div class="flex flex-grow">
-        <div class="flex-none bg-neutral-900 pt-16">
+        <div class="fixed h-screen flex-none bg-neutral-900 pt-16">
           <Sidebar />
         </div>
 
-        <div class="flex-grow overflow-y-auto pt-16">{props.children}</div>
+        <div class="flex-grow overflow-y-auto pl-32 pt-16">
+          {props.children}
+        </div>
       </div>
     </div>
   );
