8016fd4d32d4f3d73d9a19c361c17d72a194c421
Bruno <bruno.uemura1@gmail.com>
2024-02-01 23:24:21 -0300

    fix(web): hamburguer menu color

--- web/src/components/navigation/header.tsx
+++ web/src/components/navigation/header.tsx
@@ -11,7 +11,7 @@ export function Header() {
   return (
     <div className="flex items-center gap-3 text-neutral-200 bg-neutral-200 p-2 border-b dark:border-neutral-800 dark:bg-neutral-900">
       <FiMenu
-        className="text-neutral-950 hover:bg-neutral-300 dark:hover:bg-neutral-800 rounded-lg p-1 w-10 h-10 cursor-pointer"
+        className="text-neutral-950 dark:text-white hover:bg-neutral-300 dark:hover:bg-neutral-800 rounded-lg p-1 w-10 h-10 cursor-pointer"
         onClick={() => toggleSidebar()}
       />
       <Link
