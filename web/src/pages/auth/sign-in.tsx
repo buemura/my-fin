21b506e037e976cd13249d2f133fca147b6428bc
Bruno <bruno.uemura1@gmail.com>
2024-02-03 11:52:30 -0300

    feat(web): adds auth form

--- web/src/pages/auth/sign-in.tsx
+++ web/src/pages/auth/sign-in.tsx
@@ -1,74 +1,67 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { Link } from "react-router-dom";
 
-import {
-  Button,
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-  Input,
-} from "@/components/ui";
-
-const formSchema = z.object({
-  email: z.string().email({
-    message: "Email must be a valid email",
-  }),
-  password: z.string().min(8, {
-    message: "Password must contain at least 8 characters",
-  }),
-});
-
-type FormSchema = z.infer<typeof formSchema>;
+import { buttonVariants } from "@/components/ui";
+import { cn } from "@/lib/utils";
+import { ROUTES } from "@/router";
+import { SignInForm } from "./components";
 
 export function SignIn() {
-  const form = useForm<FormSchema>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
-
-  function onFormSubmit(values: FormSchema) {
-    console.log(values);
-  }
-
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onFormSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="email@email.com" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
+    <>
+      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <Link
+          to={ROUTES.SIGNUP}
+          className={cn(
+            buttonVariants({ variant: "ghost" }),
+            "absolute right-4 top-4 md:right-8 md:top-8"
           )}
-        />
+        >
+          Register
+        </Link>
 
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input type="password" placeholder="**********" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+          <div className="absolute inset-0 bg-zinc-700" />
+          <div className="relative z-20 flex items-center text-lg font-medium">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-2 h-6 w-6"
+            >
+              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+            </svg>
+            My Fin
+          </div>
+          <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg">
+                Effortlessly create and manage budgets tailored to your
+                lifestyle. Track your spending, set savings goals, and watch
+                your financial dreams become a reality.
+              </p>
+            </blockquote>
+          </div>
+        </div>
+
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Sign In with your account
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your email and password below to sign in with your account
+              </p>
+            </div>
+
+            <SignInForm />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
