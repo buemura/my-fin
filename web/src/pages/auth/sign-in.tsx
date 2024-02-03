e82064aa176871314ff4de74a863225c7a7f6ee4
Bruno <bruno.uemura1@gmail.com>
2024-02-03 12:00:48 -0300

    fix(web): auth page mobile

--- web/src/pages/auth/sign-in.tsx
+++ web/src/pages/auth/sign-in.tsx
@@ -8,7 +8,7 @@ import { SignInForm } from "./components";
 export function SignIn() {
   return (
     <>
-      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
           to={ROUTES.SIGNUP}
           className={cn(
@@ -47,8 +47,8 @@ export function SignIn() {
           </div>
         </div>
 
-        <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="h-screen lg:p-8">
+          <div className="h-full mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
                 Sign In with your account
--- web/src/pages/auth/sign-up.tsx
+++ web/src/pages/auth/sign-up.tsx
@@ -8,7 +8,7 @@ import { SignUpForm } from "./components";
 export function SignUp() {
   return (
     <>
-      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
           to={ROUTES.SIGNIN}
           className={cn(
@@ -19,7 +19,7 @@ export function SignUp() {
           Login
         </Link>
 
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+        <div className="relative h-full hidden flex-col bg-muted p-10 text-white lg:flex dark:border-r">
           <div className="absolute inset-0 bg-zinc-700" />
           <div className="relative z-20 flex items-center text-lg font-medium">
             <svg
@@ -47,8 +47,8 @@ export function SignUp() {
           </div>
         </div>
 
-        <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="h-screen lg:p-8">
+          <div className="h-full mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
                 Create an account
