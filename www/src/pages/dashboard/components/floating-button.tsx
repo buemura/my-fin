c0a91a9efbac94a86e44c1aff1361ce3af627c19
Bruno <bruno.uemura1@gmail.com>
2024-02-05 21:00:54 -0300

    feat(web): adds new resource tabs

--- www/src/pages/dashboard/components/floating-button.tsx
+++ www/src/pages/dashboard/components/floating-button.tsx
@@ -1,40 +1,31 @@
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@/components/ui";
-import { PlusIcon, TrendingDown, TrendingUp, Wallet } from "lucide-react";
+import { PlusIcon } from "lucide-react";
+import { TabsDemo } from "./create-tabs";
 
 export function FloatingButton() {
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+    <Dialog>
+      <DialogTrigger asChild>
         <div className="fixed bottom-4 right-4">
           <button className="bg-emerald-800 hover:bg-emerald-900 text-white text-center rounded-full p-4 shadow-lg">
             <PlusIcon />
           </button>
         </div>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56">
-        <DropdownMenuGroup>
-          <DropdownMenuItem className="flex gap-3 cursor-pointer">
-            <TrendingDown className="w-6 h-6 bg-red-100 dark:bg-red-950 text-red-500 dark:text-red-200 rounded-full p-1" />
-            <span>Expense</span>
-          </DropdownMenuItem>
-
-          <DropdownMenuItem className="flex gap-3 cursor-pointer">
-            <TrendingUp className="w-6 h-6 bg-emerald-100 dark:bg-emerald-950 text-emerald-500 dark:text-emerald-200 rounded-full p-1" />
-            <span>Income</span>
-          </DropdownMenuItem>
-
-          <DropdownMenuItem className="flex gap-3 cursor-pointer">
-            <Wallet className="w-6 h-6 bg-blue-100 dark:bg-blue-950 text-blue-500 dark:text-blue-200 rounded-full p-1" />
-            <span>Bank account</span>
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>New resource</DialogTitle>
+          <DialogDescription></DialogDescription>
+        </DialogHeader>
+        <TabsDemo />
+      </DialogContent>
+    </Dialog>
   );
 }
